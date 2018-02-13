0b8d1ea33d5363bd30c94905446dbafe1b50c2dd
chenjuncj <chenjuncj@didichuxing.com>
2018-02-13 10:50:51 +0800

    add function blockipfile

--- waf.lua
+++ waf.lua
@@ -3,6 +3,7 @@ local method=ngx.req.get_method()
 local ngxmatch=ngx.re.match
 if whiteip() then
 elseif blockip() then
+elseif blockipfile() then
 elseif denycc() then
 elseif ngx.var.http_Acunetix_Aspect then
     ngx.exit(444)
